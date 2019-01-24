a767140ffd6bb0c89621d4d8ec7d1603256057b0
Rinko <qw85123163@163.com>
2019-01-24 18:54:20 +0800

    修改为中文代码

--- src/main/kotlin/com.rinko/MiHoYo.kt
+++ src/main/kotlin/com.rinko/MiHoYo.kt
@@ -2,35 +2,73 @@ package com.rinko
 
 
 fun main(args: Array<String>) {
-    println(totalCost())
-    println(totalCostDays())
-    println(needCost(remainDay = 4))
+    run {
+        while (true) {
+            println("请依此输入：剩余天数 一把积分或代币 一把体力 总积分，例如：4 250 50 20000 或 4")
+            val input = readLine()!!//00.00  + 00.00
+
+            val arr = input.trim().split(" ")
+
+            val 剩余天数 = arr[0].toInt()
+//            val 一把积分或代币 = arr[1].toDouble()
+//            val 一把体力 = arr[2] .toDouble()
+//            val 总积分 = arr[3] .toDouble()
+
+            val 一体力积分 = 一体力积分()
+            val 一天体力积分 = 一天体力积分(一体力积分)
+            val 一管体力积分 = 一管体力积分(一体力积分)
+
+            val 剩余可得积分 = 剩余可得积分(剩余天数, 一体力积分)
+            val 差的积分 = 差的积分(剩余可得积分 = 剩余可得积分)
+            val 需补管数 = 需补管数(差的积分,一管体力积分)
+
+            println("一体力可以换的积分为 $一体力积分")
+            println("一天可以获得的积分为 $一天体力积分")
+            println("一管体力可以获得的积分为 $一管体力积分")
+            println("剩下的天数可以获得的积分为 $剩余可得积分")
+            println("总共还差的积分为 $差的积分")
+            println("需要补的体力的管数为 ${String.format("%.2f", 需补管数)}")
+
+            while (true) {
+                print("再来一次?[Y/N]")
+                val cmd = readLine()!!
+                if (cmd.toLowerCase() == "y") {
+                    break
+                }
+                if (cmd.toLowerCase() == "n") {
+                    return@run
+                }
+            }
+
+        }
+    }
+    println("感谢您的使用！")
 }
 
-// 当前 146
-//总积分  / 	一次积分	*  50  =  总体力
-//总积分  / 	一次积分	*  50  /  497  =  总天数
-//（总的积分-已有积分）  /   一次积分  *  50  /  167  =  需要补体力的管数
-//（总积分  / 	一次积分	*  50  -  497  *  天数  /   50  *  一次积分 ）  /  167  =  需要补的体力管数
 
-val dayGet = 497.0 //每天可支配体力 467
-val pool = 167.0  // 一管体力 167
+val 一天体力 = 490.0
+val 一管体力 = 167.0
 
-
-fun totalCost(totalPoint: Double = 20000.0, oncePoint: Double = 250.0, onceCost: Double = 50.0): Double {
-    return totalPoint / onceCost * onceCost
+fun 一体力积分(一把积分: Double = 250.0, 一把体力: Double = 50.0): Double {
+    return 一把积分 / 一把体力
 }
 
-fun totalCostDays(totalPoint: Double = 20000.0, oncePoint: Double = 250.0, onceCost: Double = 50.0): Double {
-    return totalPoint / onceCost * onceCost / dayGet
+fun 一天体力积分(一体力积分: Double): Double {
+    return 一天体力 * 一体力积分
 }
 
-//fun totalCostDays(owePoint: Double, oncePoint: Double = 250.0, onceCost: Double = 50.0): Double {
-//    return totalPoint / onceCost * onceCost / dayGet
-//}
+fun 一管体力积分(一体力积分: Double): Double {
+    return 一管体力 * 一体力积分
+}
 
+fun 剩余可得积分(天数: Int, 一体力积分: Double): Double {
+    return 天数 * 一天体力 * 一体力积分
+}
 
+fun 差的积分(总积分: Double = 20000.0, 剩余可得积分: Double): Double {
+    return 总积分 - 剩余可得积分
+}
 
-fun needCost(totalPoint: Double = 20000.0, oncePoint: Double = 250.0, onceCost: Double = 50.0, remainDay: Int): Double {
-    return (totalPoint / oncePoint * onceCost - dayGet * remainDay) / pool
+fun 需补管数(差的积分: Double,一管体力积分:Double): Double {
+    return 差的积分 / 一管体力积分
 }
